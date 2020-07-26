eaf3c16a560fdcaf93ab2ec6627cebd92da95407
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-26 22:28:01 +0600

    added 2 pages with route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import ContactInfo from './components/ContactInfo/ContactInfo';
+import Contacts from './components/Contacts/Contacts';
+import { getUsers } from './redux/rootReducer';
+import { Route } from 'react-router-dom';
+// import { getUsers } from './api/api';
+import { connect } from 'react-redux'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = React.memo((props) => {
+    // let usersData = JSON.parse(localStorage.getItem('users'))
+    // let [users, setUsers] = useState([])
+
+    useEffect(() => {
+        props.getUsers();
+    }, [props.users[0]]) 
+    return (
+        <div className="App">
+            <Header />
+            <div className="main">
+                <div className="wrapper">
+                    <Route exact path='/' render={ () => <Contacts users={ props.users }/> } />
+                    <Route path='/contact/:userId' render={ () => <ContactInfo /> } /> 
+                </div>
+            </div>
+        </div>
   );
-}
+})
 
-export default App;
+const mstp = (state) => ({
+    users: state.usersData.users
+})
+
+
+
+export default connect(mstp, { getUsers })(App);
