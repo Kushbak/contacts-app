ad84dac2eeb5c88cd0b07cd0a04884e0cb0e03ef
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-28 03:08:44 +0600

    added changing img of favorite, added hooks for seacrh, filter favorites, useEffect for get favorites from localStorage, added async func for set favorites in state and localStorage

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,22 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from './Contacts.module.css';
-import favIcon from '../../assets/images/favorite.svg'
-import nonfavIcon from '../../assets/images/nonfavorite.svg'
-import emailIcon from '../../assets/images/email.svg'
-import internetIcon from '../../assets/images/internet.svg'
-import locationIcon from '../../assets/images/location.svg'
-import smartphoneIcon from '../../assets/images/smartphone.svg'
 import { connect } from 'react-redux';
+import styles from './Contacts.module.css';
+import favIcon from '../../assets/images/favorite.svg';
+import nonfavIcon from '../../assets/images/nonfavorite.svg';
+import emailIcon from '../../assets/images/email.svg';
+import internetIcon from '../../assets/images/internet.svg';
+import locationIcon from '../../assets/images/location.svg';
+import smartphoneIcon from '../../assets/images/smartphone.svg';
+import { addToFavorites, removeFavorite, setFavoritesFromLS } from '../../redux/rootReducer';
 
-const usersMapping = (users) => {
-    return users.map(u => (
+
+const Contacts = ({ users, favorites, setFavoritesFromLS, addToFavorites, removeFavorite  }) => {
+    const [searchedUsers, setSearchedUsers] = useState();
+    const [isFavoritesFilter, setFavoritesFilter] = useState(false);  
+
+    const preventFavorites = useRef();
+    preventFavorites.current = favorites;
+    
+    useEffect(() => { 
+        if(!favorites.length && JSON.parse(localStorage.getItem('favorites')).length){
+            setFavoritesFromLS(JSON.parse(localStorage.getItem('favorites')))
+        } 
+    }, [])
+
+ 
+    
+    const setFavorites = async (user) => {   
+        if (!favorites.some(item => item.id === user.id)) {
+            await addToFavorites(user)  
+        } else {
+            await removeFavorite(user)  
+        }     
+        localStorage.setItem('favorites', JSON.stringify(preventFavorites.current));
+    }  
+
+    const searchFunc = (e) => {
+        setSearchedUsers(users.filter(item => {
+            return item.firstName.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+                || item.lastName.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+        }))
+    }
+
+    const sortFunc = (users, direction) => {  
+        [].slice.call(users).sort((a, b) => {  
+            return a > b ? direction : direction * -1;
+        }); 
+    }
+
+    return (
+        <div className={ styles.contactsBlock }>
+
+            <div className={ styles.actionBars }>
+                <div className={ styles.search }>
+                    <input type="text" className={ styles.searchInput } onChange={(e) => searchFunc(e)} placeholder='type to search...' />
+                </div>
+                <div className={ styles.actionButtons}>
+                    <span className= {styles.favoritesFilter } onClick={ () => setFavoritesFilter(!isFavoritesFilter) } ><img src={ isFavoritesFilter ? favIcon : nonfavIcon } alt="Favorites" /></span>
+                    <button onClick={ () => sortFunc(users, 1) } className={ styles.sortBtn } > AZ </button>
+                    <button onClick={ () => sortFunc(users, -1) } className={ styles.sortBtn } > ZA </button>
+                </div>
+            </div>
+
+            <div className={ styles.contacts}>
+                {searchedUsers
+                    ? <UsersMapping users={ searchedUsers } setFavorites={ setFavorites } isFavorites={ isFavoritesFilter } favorites={ favorites } />
+                    : <UsersMapping users={ users } setFavorites={ setFavorites } isFavorites={ isFavoritesFilter } favorites={ favorites } />
+                }
+            </div>
+        </div>
+    )
+}
+
+const UsersMapping = ({ users, setFavorites, isFavorites, favorites }) => {  
+    let forMapping = isFavorites ? favorites : users  
+    return forMapping.map(u => (
         <div className={ styles.contactItem } key={ u.id } >
-            <div className={ styles.photo }><img src={ u.image } alt="image" /></div>
+            <div className={ styles.photo }><img src={ u.image } alt="profile" /></div>
             <div className={ styles.textInfo }>
                 <p className={ styles.name }>
                     { u.firstName } { u.lastName }
-                    <span className={ styles.favorite }><img src={ nonfavIcon || favIcon } alt="favorite" /></span>
+                    <span className={ styles.favorite } onClick={ () => setFavorites(u) } >
+                        <img src={ (favorites.length && favorites.some(item => item.id === u.id)) ? favIcon : nonfavIcon} alt="favorite" />
+                    </span>
                 </p>
                 <p className={ styles.location }>
                     <img className={ styles.icon } src={ locationIcon } alt="icon" />
@@ -35,57 +101,17 @@ const usersMapping = (users) => {
                     { u.email }
                 </p>
                 <div className={ styles.btnBlock }>
-                    <NavLink to={`/contact/${ u.id }`} className={ styles.showUser }>Show</NavLink>
+                    <NavLink to={`/contact/${u.id}`} className={ styles.showUser }>Show</NavLink>
                 </div>
             </div>
         </div>
-    )) 
+    ))
 }
 
-const Contacts = (props) => {
-    const [searchedUsers, setSearchedUsers] = useState();
-
-    const searchFunc = (e) => {
-        setSearchedUsers(props.users.filter(item => {
-            return item.firstName.toLowerCase().search(e.target.value.toLowerCase()) != -1 
-            || item.lastName.toLowerCase().search(e.target.value.toLowerCase()) != -1
-        }))
-    } 
-
-    const azSort = (users) => {
-        return users.sort((a, b) => a.firstName - b.firstName)
-    }
-    
-    const zaSort = (users) => {
-        users.sort((a, b) => b.firstName - a.firstName)
-    }
-
-    return (
-        <div className={ styles.contactsBlock }> 
-
-            <div className={ styles.actionBars }>
-                <div className={ styles.search }>
-                    <input type="text" className={ styles.searchInput } onChange={ (e) => searchFunc(e) } placeholder='type to search...' />
-                </div>
-                <div className={ styles.actionButtons }>
-                    Favorite
-                    <button onClick={ () => azSort(props.users) }> AZ </button>
-                    <button onClick={ () => zaSort(props.users) }> ZA </button>
-                </div>
-            </div>
-
-            <div className={ styles.contacts }>
-                { searchedUsers 
-                    ? usersMapping(searchedUsers)
-                    : usersMapping(props.users)
-                }
-            </div> 
-        </div>
-    )
-}
 
 const mstp = (state) => ({
-    users: state.usersData.users
+    users: state.usersData.users,
+    favorites: state.usersData.favorites
 })
 
-export default connect(mstp, {})(Contacts);
+export default connect(mstp, { addToFavorites, removeFavorite, setFavoritesFromLS })(Contacts);
