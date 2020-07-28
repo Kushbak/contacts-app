a666ceb8e72641a54c3ba51137c670f42c5b8a00
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-29 02:35:53 +0600

    Updates

---
+++ precache-manifest.1bbe36afe922b9efe80a49322c79a05e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b086ee75ba090599c2afd840175bf426",
+    "url": "/NeobisContacts/index.html"
+  },
+  {
+    "revision": "1c936ab24d3db4f76133",
+    "url": "/NeobisContacts/static/css/main.5ecc9ce3.chunk.css"
+  },
+  {
+    "revision": "4f97fad337964e421ab8",
+    "url": "/NeobisContacts/static/js/2.f55f2326.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NeobisContacts/static/js/2.f55f2326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c936ab24d3db4f76133",
+    "url": "/NeobisContacts/static/js/main.0517dbd0.chunk.js"
+  },
+  {
+    "revision": "871580bb39ff13fc6ae1",
+    "url": "/NeobisContacts/static/js/runtime-main.2faa93ee.js"
+  },
+  {
+    "revision": "295e814c65e582886ee8680ff5fe455a",
+    "url": "/NeobisContacts/static/media/email.295e814c.svg"
+  },
+  {
+    "revision": "edffeac8c4b13f53f237c0c3c31a1e2f",
+    "url": "/NeobisContacts/static/media/favorite.edffeac8.svg"
+  },
+  {
+    "revision": "99f89efb13140b30565eba8779e1cf6f",
+    "url": "/NeobisContacts/static/media/internet.99f89efb.svg"
+  },
+  {
+    "revision": "06a791e26e0a97bbc16bc2ceda2077b2",
+    "url": "/NeobisContacts/static/media/location.06a791e2.svg"
+  },
+  {
+    "revision": "b6f07fb68bc6a6816f8b20492e089edc",
+    "url": "/NeobisContacts/static/media/logo.b6f07fb6.svg"
+  },
+  {
+    "revision": "21f320e75cd3103c36d57097eac276f2",
+    "url": "/NeobisContacts/static/media/nonfavorite.21f320e7.svg"
+  },
+  {
+    "revision": "30eebe89ab2f82f83fd7407d1ff7a281",
+    "url": "/NeobisContacts/static/media/smartphone.30eebe89.svg"
+  },
+  {
+    "revision": "c25fea94b96abbcc4570c3853a4aab70",
+    "url": "/NeobisContacts/static/media/sortAZ.c25fea94.svg"
+  },
+  {
+    "revision": "7126b71e2f734073a2bb12ef480be414",
+    "url": "/NeobisContacts/static/media/sortZA.7126b71e.svg"
+  }
+]);
