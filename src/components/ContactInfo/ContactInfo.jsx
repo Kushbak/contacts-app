ecac78f4075eeb1f2b8e3a9f8becdd00df217f88
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-29 02:29:26 +0600

    added function for submit and update users state, added useEffect for show favorites in contactDetails page, added the ability to add contact to favorite in details page

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -1,43 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import styles from './ContactInfo.module.css'
 import favIcon from '../../assets/images/favorite.svg'
+import nonfavIcon from '../../assets/images/nonfavorite.svg';
+import { addToFavorites, removeFavorite, setUsers, setFavoritesFromLS } from '../../redux/rootReducer'
+import { Input } from '../common/FormControls/FormControls';
+import { emailValid, numValid } from '../common/validators/validators';
+
 
 const ContactFormsF = (props) => { 
-    debugger
+    const submit = async (formData) => {
+        let changedData = { 
+            firstName: formData.firstName || props.user.firstName,
+            lastName: formData.lastName || props.user.lastName ,
+            city: formData.city || props.user.city,
+            country: formData.country || props.user.country,
+            phoneNumber: formData.phoneNumber || props.user.phoneNumber,
+            email: formData.email || props.user.email,
+            website: formData.website || props.user.website
+        }   
+        let updateUsers = await props.users.filter(item => item.id !== props.user.id)
+        await updateUsers.push({...props.user, ...changedData});
+        await updateUsers.sort((a, b) => a.id - b.id);
+        await props.setUsers(updateUsers)
+        localStorage.setItem('contacts', JSON.stringify(updateUsers)) 
+        for(let key in formData){
+            formData[key] = ''
+        }
+    } 
     return (
-        <div className={styles.contactForms}>
-            <form onSubmit={ props.handlerSubmit }>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>First Name:</p>
-                    <Field className={styles.contactInput} name='firstName' component='input' type='text' placeholder={ props.user.firstName } />
+        <div className={ styles.contactForms }>
+            <form onSubmit={ props.handleSubmit(submit) }>
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>First Name:</p>
+                    <Field className={ styles.contactInput } name='firstName' component={ Input } type='text' placeholder={ props.user.firstName } />
                 </div>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>Last name:</p>
-                    <Field className={styles.contactInput} name='lastName' component='input' type='text' placeholder={ props.user.lastName } />
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>Last name:</p>
+                    <Field className={ styles.contactInput } name='lastName' component={ Input } type='text' placeholder={ props.user.lastName } />
                 </div>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>City:</p>
-                    <Field className={styles.contactInput} name='city' component='input' type='text' placeholder={ props.user.city } />
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>City:</p>
+                    <Field className={ styles.contactInput } name='city' component={ Input } type='text' placeholder={ props.user.city } />
                 </div>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>Country:</p>
-                    <Field className={styles.contactInput} name='country' component='input' type='text' placeholder={ props.user.country } />
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>Country:</p>
+                    <Field className={ styles.contactInput } name='country' component={ Input } type='text' placeholder={ props.user.country } />
                 </div>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>Phone Number:</p>
-                    <Field className={styles.contactInput} name='phoneNumber' component='input' type='text' placeholder={ props.user.phoneNumber } />
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>Phone Number:</p>
+                    <Field className={ styles.contactInput } name='phoneNumber' component={ Input } type='text' placeholder={ props.user.phoneNumber } validate={ [numValid] } />
                 </div>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>Email:</p>
-                    <Field className={styles.contactInput} name='email' component='input' type='text' placeholder={ props.user.email } />
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>Email:</p>
+                    <Field className={ styles.contactInput } name='email' component={ Input } type='text' placeholder={ props.user.email } validate={ [emailValid] }/>
                 </div>
-                <div className={styles.inputBlock}>
-                    <p className={styles.inputTitle}>Website:</p>
-                    <Field className={styles.contactInput} name='website' component='input' type='text' placeholder={ props.user.website } />
+                <div className={ styles.inputBlock }>
+                    <p className={ styles.inputTitle }>Website:</p>
+                    <Field className={ styles.contactInput } name='website' component={ Input } type='text' placeholder={ props.user.website } />
                 </div>
                 <button className={ styles.saveContact } >Save Contact</button>
             </form>
@@ -46,25 +69,44 @@ const ContactFormsF = (props) => {
 }
 const ContactForms = reduxForm({ form: 'contactDetails' })(ContactFormsF)
 
-const ContactInfo = (props) => { 
-
+const ContactInfo = (props) => {  
     const [userId, setUserId] = useState()
 
     useEffect(() => { 
-        setUserId(props.match.params.userId);  
-    })
+        if(!props.favorites.length && JSON.parse(localStorage.getItem('favorites')).length){
+            props.setFavoritesFromLS(JSON.parse(localStorage.getItem('favorites')))
+        } 
+    }, [])
+
+    const preventFavorites = useRef()
+    preventFavorites.current = props.favorites
+
+    useEffect(() => { 
+        setUserId(props.match.params.userId);   
+    }, [])
+ 
+
+    const setFavorites = async (user) => {   
+        if (!props.favorites.some(item => item.id === user.id)) {
+            await props.addToFavorites(user)  
+        } else {
+            await props.removeFavorite(user)  
+        }     
+        localStorage.setItem('favorites', JSON.stringify(preventFavorites.current))
+    }  
+    
     return (
         <div className={ styles.contact }>
             { props.users.map(u => {
-                if(u.id == userId){
+                if(u.id === +userId){
                     return (
-                        <div className={ styles.contactInfo }>
+                        <div className={ styles.contactInfo } key={ u.id } >
                             <div className={ styles.imgBlock }>
-                                <img src={ u.image } alt="photo" className={ styles.photo } />
-                                <img src={ favIcon } alt="fav" className={ styles.favIcon }/>
+                                <img src={ u.image } alt="profile" className={ styles.photo } />
+                                <img src={ (props.favorites.length && props.favorites.some(item => item.id === u.id)) ? favIcon : nonfavIcon } onClick={ () => setFavorites(u) } alt="fav" className={ styles.favIcon }/>
                             </div>
                             
-                            <ContactForms user={ u }/>
+                            <ContactForms user={ u } users={ props.users } setUsers={ props.setUsers } />
 
                         </div>
                     )
@@ -75,10 +117,11 @@ const ContactInfo = (props) => {
 }
 
 const mstp = (state) => ({
-    users: state.usersData.users
+    users: state.usersData.users,
+    favorites: state.usersData.favorites
 })
 
 export default  compose( 
 withRouter,
-connect(mstp, {})
+connect(mstp, { addToFavorites, removeFavorite, setUsers, setFavoritesFromLS })
 )(ContactInfo);
